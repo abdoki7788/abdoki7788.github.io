49fce06b90708235cdab295ce811cc3ffda04240
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-04-21 22:28:29 +0430

    profile section added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+        "./src/*.{html,js}",
+        "./src/scripts/*.{html,js}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          'ravi': ['Ravi']
+        }
+      },
+    },
+    plugins: [],
+  }
+  
