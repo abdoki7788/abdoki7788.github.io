6dc6f9652de10093d3701dbfdbf8b255350ff2bb
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-04-22 15:07:57 +0430

    navbar and about-me section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-    content: [
-        "./src/*.{html,js}",
-        "./src/scripts/*.{html,js}",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          'ravi': ['Ravi']
-        }
-      },
+  darkMode: 'class',
+  content: [
+      "./src/*.{html,js}",
+      "./src/scripts/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'ravi': ['Ravi'],
+        'shabnam': ['Shabnam'],
+        'shabnam-bold': ['Shabnam-Bold'],
+        'shabnam-medium': ['Shabnam-Medium'],
+        'shabnam-light': ['Shabnam-Light'],
+      }
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
